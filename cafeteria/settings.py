47f43bef1b294d5987a82a6cac9c27c9e571c747
varun-singhh <47475007+varun-singhh@users.noreply.github.com>
2020-09-14 09:33:15 -0500

    Update settings.py
    SMTP UPDATE

--- cafeteria/settings.py
+++ cafeteria/settings.py
@@ -125,6 +125,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_HOST='smtp.gmail.com'
 EMAIL_PORT=587
 EMAIL_HOST_USER='varunworks27@gmail.com'
-EMAIL_HOST_PASSWORD='9044375672'
+EMAIL_HOST_PASSWORD='*********'
 EMAIL_USE_TLS=True
 
